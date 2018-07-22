a1de8e73f45030b084aae0d915e3b934e0caf5ba
inPhoenix <spiderk@gmail.com>
2018-07-22 09:24:44 +0100

    Committing fixes in the dark

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,25 @@
 // Import flutter helper library
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
+        body: Text('$counter'),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: () {
-            print('hi there');
+            setState(() {
+              counter += 1;
+            });
           },
         ),
         appBar: AppBar(
